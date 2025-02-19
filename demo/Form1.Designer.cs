2a69a45d999867996f439a64ccf0b39dcab964ba
tai71c4 <nhantaivang9999@gmail.com>
2025-02-19 14:11:08 +0700

    up
    up

---
+++ demo/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FastFoodOrder
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMenu = new System.Windows.Forms.DataGridView();
+            this.dgvOrder = new System.Windows.Forms.DataGridView();
+            this.btnAddItem = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnPrintReceipt = new System.Windows.Forms.Button();
+            this.txtDiscountCode = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblDiscountStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMenu
+            // 
+            this.dgvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenu.Location = new System.Drawing.Point(79, 39);
+            this.dgvMenu.Name = "dgvMenu";
+            this.dgvMenu.RowHeadersWidth = 51;
+            this.dgvMenu.RowTemplate.Height = 24;
+            this.dgvMenu.Size = new System.Drawing.Size(476, 179);
+            this.dgvMenu.TabIndex = 0;
+            // 
+            // dgvOrder
+            // 
+            this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrder.Location = new System.Drawing.Point(79, 328);
+            this.dgvOrder.Name = "dgvOrder";
+            this.dgvOrder.RowHeadersWidth = 51;
+            this.dgvOrder.RowTemplate.Height = 24;
+            this.dgvOrder.Size = new System.Drawing.Size(597, 156);
+            this.dgvOrder.TabIndex = 0;
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Location = new System.Drawing.Point(461, 233);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(94, 32);
+            this.btnAddItem.TabIndex = 1;
+            this.btnAddItem.Text = "Thêm món";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(338, 233);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(94, 32);
+            this.btnRemoveItem.TabIndex = 1;
+            this.btnRemoveItem.Text = "Xóa món";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(79, 508);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(63, 16);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Tổng tiền";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // btnPrintReceipt
+            // 
+            this.btnPrintReceipt.Location = new System.Drawing.Point(525, 562);
+            this.btnPrintReceipt.Name = "btnPrintReceipt";
+            this.btnPrintReceipt.Size = new System.Drawing.Size(151, 42);
+            this.btnPrintReceipt.TabIndex = 3;
+            this.btnPrintReceipt.Text = "Thanh Toán và in hóa đơn";
+            this.btnPrintReceipt.UseVisualStyleBackColor = true;
+            this.btnPrintReceipt.Click += new System.EventHandler(this.btnPrintReceipt_Click);
+            // 
+            // txtDiscountCode
+            // 
+            this.txtDiscountCode.Location = new System.Drawing.Point(80, 582);
+            this.txtDiscountCode.Name = "txtDiscountCode";
+            this.txtDiscountCode.Size = new System.Drawing.Size(123, 22);
+            this.txtDiscountCode.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(80, 11);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(475, 22);
+            this.txtSearch.TabIndex = 6;
+            // 
+            // lblDiscountStatus
+            // 
+            this.lblDiscountStatus.AutoSize = true;
+            this.lblDiscountStatus.Location = new System.Drawing.Point(79, 563);
+            this.lblDiscountStatus.Name = "lblDiscountStatus";
+            this.lblDiscountStatus.Size = new System.Drawing.Size(81, 16);
+            this.lblDiscountStatus.TabIndex = 7;
+            this.lblDiscountStatus.Text = "Mã giảm giá";
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 685);
+            this.Controls.Add(this.lblDiscountStatus);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtDiscountCode);
+            this.Controls.Add(this.btnPrintReceipt);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.btnAddItem);
+            this.Controls.Add(this.dgvOrder);
+            this.Controls.Add(this.dgvMenu);
+            this.Name = "OrderForm";
+            this.RightToLeftLayout = true;
+            this.Text = "S";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMenu;
+        private System.Windows.Forms.DataGridView dgvOrder;
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.Button btnRemoveItem;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnPrintReceipt;
+        private System.Windows.Forms.TextBox txtDiscountCode;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblDiscountStatus;
+    }
+}
+
---
+++ demo/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace FastFoodOrder
+{
+    public partial class OrderForm : Form
+    {
+        private List<MenuItem> menu;
+        private List<OrderItem> orderItems = new List<OrderItem>();
+        private PrintDocument printDocument = new PrintDocument();
+        private decimal discount = 0;
+        private decimal vatRate = 0.1m;
+
+        // Danh sách mã giảm giá hợp lệ
+        private Dictionary<string, decimal> discountCodes = new Dictionary<string, decimal>
+        {
+            { "SALE10", 0.10m },  // Giảm 10%
+            { "SALE20", 0.20m },  // Giảm 20%
+            { "SALE30", 0.30m },  // Giảm 30%
+            { "SALEVL", 0.99m }   // Giảm VL%
+        };
+
+        public OrderForm()
+        {
+            InitializeComponent();
+            LoadMenuItems();
+            printDocument.PrintPage += new PrintPageEventHandler(PrintReceipt);
+            txtSearch.TextChanged += TxtSearch_TextChanged;
+            txtDiscountCode.TextChanged += TxtDiscountCode_TextChanged; // Bắt sự kiện nhập mã giảm giá
+        }
+
+        private void LoadMenuItems()
+        {
+            menu = new List<MenuItem>
+            {
+                new MenuItem("B001", "Burger Gà", 50000),
+                new MenuItem("B002", "Burger Bò", 55000),
+                new MenuItem("D001", "Coca Cola", 15000),
+                new MenuItem("D002", "Pepsi", 15000),
+                new MenuItem("D003", "Trà Sữa Trân Châu", 40000),
+                new MenuItem("D004", "Sinh Tố Bơ", 45000),
+                new MenuItem("B003", "Burger Cá", 52000),
+                new MenuItem("B004", "Burger Phô Mai", 58000),
+                new MenuItem("S001", "Khoai Tây Chiên", 30000),
+                new MenuItem("S002", "Gà Rán", 60000),
+                new MenuItem("S003", "Hot Dog", 40000),
+                new MenuItem("S004", "Pizza Phô Mai", 120000),
+                new MenuItem("S005", "Mì Ý Sốt Bò Bằm", 90000)
+            };
+            dgvMenu.DataSource = menu;
+        }
+
+        private void TxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = txtSearch.Text.ToLower();
+            dgvMenu.DataSource = menu.Where(m => m.Name.ToLower().Contains(searchText) || m.Code.ToLower().Contains(searchText)).ToList();
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            if (dgvMenu.SelectedRows.Count > 0)
+            {
+                var selectedRow = dgvMenu.SelectedRows[0];
+                var itemCode = selectedRow.Cells["Code"].Value.ToString();
+                var itemName = selectedRow.Cells["Name"].Value.ToString();
+                var itemPrice = Convert.ToDecimal(selectedRow.Cells["Price"].Value);
+
+                var existingItem = orderItems.FirstOrDefault(i => i.Code == itemCode);
+                if (existingItem != null)
+                    existingItem.Quantity++;
+                else
+                    orderItems.Add(new OrderItem(itemCode, itemName, itemPrice, 1));
+
+                RefreshOrderList();
+            }
+        }
+
+        private void RefreshOrderList()
+        {
+            dgvOrder.DataSource = null;
+            dgvOrder.DataSource = orderItems;
+            lblTotal.Text = $"Tổng tiền: {CalculateFinalTotal():N0} VND(đã bao gồm phí VAT)";
+        }
+
+        private decimal CalculateTotal()
+        {
+            return orderItems.Sum(item => item.Price * item.Quantity);
+        }
+
+        private decimal CalculateFinalTotal()
+        {
+            decimal total = CalculateTotal();
+            total -= total * discount; // Trừ giảm giá
+            total += total * vatRate;  // Thêm VAT
+            return total;
+        }
+
+        private void TxtDiscountCode_TextChanged(object sender, EventArgs e)
+        {
+            string enteredCode = txtDiscountCode.Text.Trim().ToUpper();
+
+            if (discountCodes.ContainsKey(enteredCode))
+            {
+                discount = discountCodes[enteredCode];
+                lblDiscountStatus.Text = $"Mã hợp lệ! Giảm {discount * 100}%";
+            }
+            else
+            {
+                discount = 0;
+                lblDiscountStatus.Text = "Mã không hợp lệ!";
+            }
+
+            RefreshOrderList(); // Cập nhật lại tổng tiền
+        }
+
+        private void btnPrintReceipt_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog { Document = printDocument };
+            if (printDialog.ShowDialog() == DialogResult.OK)
+                printDocument.Print();
+        }
+
+        private void PrintReceipt(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial", 12);
+            float y = 20;
+            g.DrawString("HÓA ĐƠN THANH TOÁN", new Font("Arial", 16, FontStyle.Bold), Brushes.Black, 80, y);
+            y += 40;
+            foreach (var item in orderItems)
+            {
+                g.DrawString($"{item.Name} x {item.Quantity} - {item.Price * item.Quantity:N0} VND", font, Brushes.Black, 20, y);
+                y += 30;
+            }
+            y += 20;
+            g.DrawString($"Tổng tiền: {CalculateTotal():N0} VND", font, Brushes.Black, 20, y);
+            y += 30;
+            g.DrawString($"Giảm giá: {discount * 100:N0}%", font, Brushes.Black, 20, y);
+            y += 30;
+            g.DrawString($"VAT: {vatRate * 100:N0}%", font, Brushes.Black, 20, y);
+            y += 30;
+            g.DrawString($"Thành tiền: {CalculateFinalTotal():N0} VND", new Font("Arial", 14, FontStyle.Bold), Brushes.Black, 20, y);
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class MenuItem
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public MenuItem(string code, string name, decimal price) => (Code, Name, Price) = (code, name, price);
+    }
+
+    public class OrderItem : MenuItem
+    {
+        public int Quantity { get; set; }
+        public OrderItem(string code, string name, decimal price, int quantity) : base(code, name, price) => Quantity = quantity;
+    }
+}
